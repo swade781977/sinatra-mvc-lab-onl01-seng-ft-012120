5ae9027665668cf943cbc6190213ccf5c72d713f
Chris Wade <swade781977@yahoo.com>
2020-04-20 18:27:06 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,26 +1,29 @@
 class PigLatinizer
-  attr_reader :text
+  attr_accessor :text
   
-  def initialize
-    @text = text
-  end
+  
   
   def split_phrase
     words = @text.split(" ")
   end
   
-  def piglatinize
+  def piglatinize(text)
+    words = text.split(" ")
+    result = []
     words.each do |word|
-      if word[0].include?(/[aeoui]/)
+       word_v = word[0].scan(/[aeoui]/).count
+      if word_v > 0
         new_word = word + "way"
+        result << new_word
       end
-      if word[0].include?(/[bcdfghjklmnpqrstvwxyz]/)
+      if word_v == 0
         first_letter = word[0]
-        alt_word = word[1...-1]
+        alt_word = word[1..-1]
         new_word = alt_word + first_letter + "ay"
+        result << new_word
       end
       word = new_word
     end
-    words
+    result.join(' ')
   end
 end
