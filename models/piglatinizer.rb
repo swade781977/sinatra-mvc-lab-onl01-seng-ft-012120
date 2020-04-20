2fd3bc6d68b8cad8aecda0719ca0281322004cf2
Chris Wade <swade781977@yahoo.com>
2020-04-20 18:45:48 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,13 +14,15 @@ class PigLatinizer
     words = text.split(" ")
     result = []
     words.each do |word|
-       word_v = word[0].scan(/[aeouiAIEOU]/).count
-      if word_v > 0
+       word_v = word[0].include?(/[aeouiAIEOU]/)
+       #word_v = word[0].scan(/[aeouiAIEOU]/).count
+      if word_v 
         new_word = word + "way"
         result << new_word
       end
       if word_v == 0
         first_letter = word[0]
+        if word[1] 
         alt_word = word[1..-1]
         new_word = alt_word + first_letter + "ay"
         result << new_word
